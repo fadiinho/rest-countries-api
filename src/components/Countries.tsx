d89e1a993b5e4377c94636b49e917c18e98c1789
fadiinho <ffadiinho@gmail.com>
2022-10-28 20:16:31 -0300

    feat: fix responsivity

--- src/components/Countries.tsx
+++ src/components/Countries.tsx
@@ -49,35 +49,39 @@ export const Countries = () => {
   }, [countries, filter]);
 
   return (
-    <div className="p-4 flex flex-col items-center justify-center ">
-      <div className="m-4 w-full bg-white dark:bg-dark-blue flex rounded drop-shadow">
-        <MagnifyingGlassIcon onClick={search} className="w-6 h-6 m-4" />
-        <input
-          className="w-full min-h-full bg-white dark:bg-dark-blue rounded"
-          placeholder="Search for a country..."
-          onChange={(event) => setValue(event.target.value)}
-          value={value}
-          onKeyDown={handleKeyDown}
-        />
-      </div>
+    <div className="p-4 sm:w-full flex flex-col items-center justify-center ">
+      <div className="w-full flex flex-col justify-between sm:flex-row gap-8">
+        <div className="sm:w-1/3 bg-white dark:bg-dark-blue flex rounded drop-shadow">
+          <MagnifyingGlassIcon onClick={search} className="w-6 h-6 m-4" />
+          <input
+            className="w-full min-h-full bg-white dark:bg-dark-blue rounded"
+            placeholder="Search for a country..."
+            onChange={(event) => setValue(event.target.value)}
+            value={value}
+            onKeyDown={handleKeyDown}
+          />
+        </div>
 
-      <div className="m-4 w-full bg-white dark:bg-dark-blue flex items-center relative rounded drop-shadow">
-        <select
-          className="p-4 w-full min-h-full bg-white dark:bg-dark-blue rounded"
-          onChange={(event) => setFilter(event.target.value)}
-          value={filter}
-        >
-          <option value="" disabled hidden>Filter by Region</option>
-          <option value="africa">Africa</option>
-          <option value="americas">Americas</option>
-          <option value="asia">Asia</option>
-          <option value="europe">Europe</option>
-          <option value="oceania">Oceania</option>
-        </select>
-        {filter && <XCircleIcon onClick={() => setFilter("")} className="w-8 h-8 absolute right-0 cursor-pointer" />}
+        <div className="sm:m-0 bg-white dark:bg-dark-blue flex items-center self-start sm:self-center relative rounded drop-shadow">
+          <select
+            className="p-4 min-h-full bg-white dark:bg-dark-blue rounded"
+            onChange={(event) => setFilter(event.target.value)}
+            value={filter}
+          >
+            <option value="" disabled hidden>Filter by Region</option>
+            <option value="africa">Africa</option>
+            <option value="americas">Americas</option>
+            <option value="asia">Asia</option>
+            <option value="europe">Europe</option>
+            <option value="oceania">Oceania</option>
+          </select>
+          <XCircleIcon onClick={() => setFilter("")} className={`w-6 h-6 cursor-pointer ${filter ? "opacity-1" : "opacity-0"}`} />
+        </div>
       </div>
-      {!error && filteredCountries.map((country) => <CountryCard key={country.name} {...country} />)}
-      {error && <Error error={error}/>}
+      <div className="m-8 p-4 sm:m-8 w-full flex flex-col justify-center gap-8 sm:flex-row sm:flex-wrap">
+        {!error && filteredCountries.map((country) => <CountryCard key={country.name} {...country} />)}
+        {error && <Error error={error}/>}
+      </div>
     </div>
   )
 };
--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -2,16 +2,16 @@ import type { Country } from "../../types/utils";
 
 const CardInfo = ({ infoName, infoText }: { infoName: string, infoText: string  }) => {
   return (
-    <p className="m-2 mx-4 font-semibold">{infoName}: <span className="font-light">{infoText}</span></p>
+    <p className="m-2 font-semibold">{infoName}: <span className="font-light">{infoText}</span></p>
   )
 }
 
 export const CountryCard = ({ name, flag, population, region, capital }: Country) => {
   return (
-    <div className="m-8 rounded overflow-hidden bg-white dark:bg-dark-blue drop-shadow-xl">
-      <img src={flag} alt={`${name} flag`} className="" />
-      <div id="info" className="py-4 px-2">
-        <h2 className="m-4 font-bold text-xl">{name}</h2> 
+    <div className="max-w-32 rounded overflow-hidden bg-white dark:bg-dark-blue flex flex-col drop-shadow-xl">
+      <img id={name} src={flag} alt={`${name} flag`} className="grow" />
+      <div id="info" className="pt-2 px-2">
+        <h2 className="m-2 font-bold text-xl">{name}</h2>
         <CardInfo infoName="Population" infoText={population.toLocaleString("en-us")} />
         <CardInfo infoName="Region" infoText={region} />
         <CardInfo infoName="Capital" infoText={capital} />
